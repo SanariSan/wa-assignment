362f77e97fd96eeb516dd211c621fc5fa624590f
SanariSan <SanariSan@users.noreply.github.com>
2023-06-20 15:50:26 +0400

    ci: fix

--- frontend/deploy-entrypoint-apply.sh
+++ frontend/deploy-entrypoint-apply.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=wa-static;
 
-docker run --rm \
+docker run --detach --rm \
 -v "${PROJECT_NAME}-front-build-volume:/old-build" \
 busybox sh -c "rm -rf /old-build/*";
 
@@ -10,5 +10,9 @@ docker run --detach --rm \
 -v "$(pwd)/build-volume:/from-local-folder" \
 -v "${PROJECT_NAME}-front-build-volume:/to-inside-volume" \
 busybox sh -c "cp -rf /from-local-folder/* /to-inside-volume";
+
+docker run --detach --rm \
+-v "${PROJECT_NAME}-front-build-volume:/current-build" \
+busybox sh -c "chown -R 101:101 /current-build";
 
 exit;
